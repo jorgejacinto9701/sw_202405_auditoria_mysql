17ce0baced46bfba17e28c5d9ca8d4e5eab9df64
jorgejacinto9701 <jorgejacinto9701@gmail.com>
2024-10-12 19:17:45 -0500

    inicio

---
+++ src/main/java/com/example/auditoria/entity/Auditoria.java
@@ -0,0 +1,36 @@
+package com.example.auditoria.entity;
+
+import java.util.Date;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.persistence.Temporal;
+import jakarta.persistence.TemporalType;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "auditoria")
+public class Auditoria {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int idAuditoria;
+
+    @Column(name = "tipoBaseDatos", length = 50, nullable = false)
+    private String tipoBaseDatos;
+    @Column(name = "nombreBaseDatos", length = 200, nullable = false)
+    private String nombreBaseDatos;
+    @Column(name = "nombreTabla", length = 200, nullable = false)
+    private String nombreTabla;
+
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date fechaRegistro;
+    
+}
